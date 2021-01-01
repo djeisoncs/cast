17928a87e231ff56945aeb72c8752c7e82bb4122
djeisonCassimiro <djeison.dev@gmail.com>
2020-12-31 23:57:13 -0300

    inicio da parte de controller

--- src/main/java/br/com/cast/avaliacao/controller/interfaces/IController.java
+++ src/main/java/br/com/cast/avaliacao/controller/interfaces/IController.java
@@ -1,7 +1,22 @@
 package br.com.cast.avaliacao.controller.interfaces;
 
+import br.com.cast.avaliacao.util.NegocioException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.Response;
+
 /**
  * Created by djeison.cassimiro on 28/12/2020
  */
 public class IController {
+
+    @Autowired
+    private HttpServletResponse response;
+
+    public Response getResponseExcepetion(NegocioException e) {
+
+    }
+
+    public Response getResponseExcepetion(Response.)
 }
