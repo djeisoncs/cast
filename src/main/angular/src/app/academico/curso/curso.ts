a01d939e16049d92d5ad5596887661af7a22f49d
djeisonCassimiro <djeison.dev@gmail.com>
2021-01-10 18:14:42 -0300

    configuração da arquitetura do projeto angular

--- src/main/angular/src/app/academico/curso/curso.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by djeison.cassimiro on 09/01/2021
- */
-import {Entidade} from "../../core/model/Entidade";
-
-export class Curso extends Entidade {
-
-  nome: string;
-}
--- src/main/angular/src/app/core/model/Entidade.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by djeison.cassimiro on 09/01/2021
- */
-import {Status} from "../enum/status";
-
-export class Entidade {
-  id: string;
-  status: Status;
-}
